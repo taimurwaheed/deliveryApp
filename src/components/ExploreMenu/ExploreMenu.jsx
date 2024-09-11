071a0b89bd116ea27801884ebc4963420342cd38
My name <me@example.com>
2024-09-11 23:01:12 +0500

    first commit

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our Menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in numquam
+        qui animi, laboriosam repudiandae temporibus possimus eos totam,
+        voluptatibus libero suscipit quia illum iste assumenda molestiae
+        reprehenderit. Iste, quae.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-items"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+          maiores nulla ipsa ut unde cumque aliquam quae delectus nobis? Ipsa
+          quod eligendi iure, suscipit eius perferendis placeat blanditiis iusto
+          maxime!
+        </p>
+        <button>view menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
